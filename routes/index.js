6895fd0f6240dc3e16a230e10166d2db56808d73
FranSalguero <fransalguero@gmail.com>
2015-06-27 14:13:01 +0200

    Primera pregunta añadida

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+//instalamos el controlador de preguntas
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+//rutas nuevas para preguntas y respuestas
+router.get('quizes/question', quizController.question);
+router.get('quizes/answer', quizController.answer);
 
 module.exports = router;
