370a2dfd0b60db5ef976e86fd2dd55873937392a
FranSalguero <fransalguero@gmail.com>
2015-07-17 18:21:43 +0200

    Arreglado, faltaba una barrita en el router

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/new',                  quizController.new);
-router.post('quizes/create',               quizController.create);
+router.post('/quizes/create',               quizController.create);
 
 //GET /author
 router.get('/author', function(req, res) {
