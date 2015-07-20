1c2cfc5b3ee92db71946e418c0c4dc42252d283a
FranSalguero <fransalguero@gmail.com>
2015-07-20 19:47:48 +0200

    Módulo 8.2: Validación de entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -8,9 +8,9 @@ exports.load = function(req, res, next, quizId) {
       if (quiz) {
         req.quiz = quiz;
         next();
-      } else { next(new Error('No existe quizId = ' + quizId)); }
+      } else { next(new Error('No existe quizId = ' + quizId)) }
     }
-  ).catch(function(error) { next(error);});
+  ).catch(function(error) { next(error)});
 };
 
 // GET /quizes?search=busqueda
@@ -18,29 +18,29 @@ exports.index = function(req, res) {
   //var temporal = '%' + replace(req.quiz.search, ' ', '%') +'%';
   if (req.query.search===undefined) {
     models.Quiz.findAll().then(function(quizes) {
-      res.render('quizes/index.ejs', { quizes: quizes});
-    }).catch(function(error) { next(error);})
+      res.render('quizes/index.ejs', { quizes: quizes, errors: []});
+    }).catch(function(error) { next(error)});
  }
  else{
    models.Quiz.findAll({where: ["pregunta like ?", "%" + req.query.search.replace(" " , "%") +"%"]}).then(function(quizes) {
-     res.render('quizes/index.ejs', { quizes: quizes});
-   }).catch(function(error) { next(error);})
+     res.render('quizes/index.ejs', { quizes: quizes, errors: []});
+   }).catch(function(error) { next(error)});
  }
 };
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-  res.render('quizes/show', { quiz: req.quiz});
+  res.render('quizes/show', { quiz: req.quiz, errors: []});
 };
 
 // GET /quizes/:id/answer
 exports.answer = function(req, res) {
   if (req.query.respuesta === req.quiz.respuesta) {
     res.render('quizes/answer',
-               { quiz: req.quiz, respuesta: 'Correcto' });
+               { quiz: req.quiz, respuesta: 'Correcto' , errors: []});
   } else {
     res.render('quizes/answer',
-               { quiz: req.quiz, respuesta: 'Incorrecto'});
+               { quiz: req.quiz, respuesta: 'Incorrecto' , errors: []});
   }
 };
 
@@ -50,7 +50,7 @@ exports.new = function(req, res) {
     {pregunta: "Pregunta", respuesta: "Respuesta"}
   );
 
-  res.render('quizes/new', {quiz: quiz});
+  res.render('quizes/new', {quiz: quiz , errors: []});
 };
 
 // POST /quizes/create
@@ -58,7 +58,17 @@ exports.create = function(req, res) {
   var quiz = models.Quiz.build( req.body.quiz );
 
 // guarda en DB los campos pregunta y respuesta de quiz
-  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-    res.redirect('/quizes');
-  })   // res.redirect: Redirección HTTP a lista de preguntas
+  quiz
+  .validate()
+  .then(
+    function(err){
+      if (err) {
+        res.render('quizes/new', {quiz: quiz, errors: err.errors});
+      }else{
+          quiz
+          .save({fields: ["pregunta", "respuesta"]})
+          .then( function(){ res.redirect('/quizes')})   // res.redirect: Redirección HTTP a lista de preguntas
+      }
+    }
+  );
 };
