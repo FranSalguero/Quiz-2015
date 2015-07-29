bb6126fd812d020b42cc5576bf565d9abbe66532
FranSalguero <fransalguero@gmail.com>
2015-07-29 19:51:27 +0200

    Módulo 9.2. Autenticación y sesión

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 //y el de comentarios
 var commentController = require('../controllers/comment_controller');
+//y ahora el de sesiones
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -13,6 +15,11 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+
+// Definición de rutas de sesión
+router.get('/login',  sessionController.new);     // formulario login
+router.post('/login', sessionController.create);  // crear sesión
+router.get('/logout', sessionController.destroy); // destruir sesión
 
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
@@ -23,7 +30,7 @@ router.post('/quizes/create',               quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
 router.put('/quizes/:quizId(\\d+)',        quizController.update);
 router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
-
+//rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new',            commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',              commentController.create);
 
